d9e4015ec9f6171c5bf386e3a1f3fb96334408a7
Kyle Mitchell <kyle@kemitchell.com>
2015-04-13 23:16:25 -0700

    refactor tests

--- test/outline-numbers.test.js
+++ test/outline-numbers.test.js
@@ -2,91 +2,96 @@
 var expect = require('chai').expect;
 var outline = require('..');
 
-var xofy = function(x, y) {
-  return {number: x, of: y};
-};
-
 describe('Outline style', function() {
   describe('provision numbering', function() {
     it('uses Arabic numerals for top-level', function() {
-      expect(outline.provision([
-        {series: xofy(1, 1), element: xofy(1, 1)}
-      ]))
-        .to.equal('1.');
+      expect(
+        outline.provision([
+          {series: {number: 1, of: 1}, element: {number: 1, of: 1}}
+        ])
+      ).to.equal('1.');
     });
 
     it('uses lower-alpha numerals for second-level', function() {
-      expect(outline.provision([
-        {series: xofy(1, 1), element: xofy(1, 1)},
-        {series: xofy(1, 1), element: xofy(1, 1)}
-      ]))
-        .to.equal('(a)');
+      expect(
+        outline.provision([
+          {series: {number: 1, of: 1}, element: {number: 1, of: 1}},
+          {series: {number: 1, of: 1}, element: {number: 1, of: 1}}
+        ])
+      ).to.equal('(a)');
     });
 
     it('uses alphabetic prefixes for multiple series', function() {
-      expect(outline.provision([
-        {series: xofy(1, 2), element: xofy(1, 1)}
-      ]))
-        .to.equal('A-1.');
+      expect(
+        outline.provision([
+          {series: {number: 1, of: 2}, element: {number: 1, of: 1}}
+        ])
+      ).to.equal('A-1.');
     });
   });
 
   describe('reference numbering', function() {
     it('uses Arabic numerals for first-level', function() {
-      expect(outline.reference([
-        {series: xofy(1, 1), element: xofy(1, 1)}
-      ]))
-        .to.equal('Section 1');
+      expect(
+        outline.reference([
+          {series: {number: 1, of: 1}, element: {number: 1, of: 1}}
+        ])
+      ).to.equal('Section 1');
     });
 
     it('uses lower-alpha numerals for second-level', function() {
-      expect(outline.reference([
-        {series: xofy(1, 1), element: xofy(1, 1)},
-        {series: xofy(1, 1), element: xofy(1, 1)}
-      ]))
-        .to.equal('Section 1(a)');
+      expect(
+        outline.reference([
+          {series: {number: 1, of: 1}, element: {number: 1, of: 1}},
+          {series: {number: 1, of: 1}, element: {number: 1, of: 1}}
+        ])
+      ).to.equal('Section 1(a)');
     });
 
     it('uses lower-roman numerals for third-level', function() {
-      expect(outline.reference([
-        {series: xofy(1, 1), element: xofy(1, 1)},
-        {series: xofy(1, 1), element: xofy(1, 1)},
-        {series: xofy(1, 1), element: xofy(1, 1)}
-      ]))
-        .to.equal('Section 1(a)(i)');
+      expect(
+        outline.reference([
+          {series: {number: 1, of: 1}, element: {number: 1, of: 1}},
+          {series: {number: 1, of: 1}, element: {number: 1, of: 1}},
+          {series: {number: 1, of: 1}, element: {number: 1, of: 1}}
+        ])
+      ).to.equal('Section 1(a)(i)');
     });
 
     it('uses upper-alpha numerals for fourth-level', function() {
-      expect(outline.reference([
-        {series: xofy(1, 1), element: xofy(1, 1)},
-        {series: xofy(1, 1), element: xofy(1, 1)},
-        {series: xofy(1, 1), element: xofy(1, 1)},
-        {series: xofy(1, 1), element: xofy(1, 1)}
-      ]))
-        .to.equal('Section 1(a)(i)(A)');
+      expect(
+        outline.reference([
+          {series: {number: 1, of: 1}, element: {number: 1, of: 1}},
+          {series: {number: 1, of: 1}, element: {number: 1, of: 1}},
+          {series: {number: 1, of: 1}, element: {number: 1, of: 1}},
+          {series: {number: 1, of: 1}, element: {number: 1, of: 1}}
+        ])
+      ).to.equal('Section 1(a)(i)(A)');
     });
 
     it('uses upper-Roman numerals for fifth-level', function() {
-      expect(outline.reference([
-        {series: xofy(1, 1), element: xofy(1, 1)},
-        {series: xofy(1, 1), element: xofy(1, 1)},
-        {series: xofy(1, 1), element: xofy(1, 1)},
-        {series: xofy(1, 1), element: xofy(1, 1)},
-        {series: xofy(1, 1), element: xofy(1, 1)}
-      ]))
-        .to.equal('Section 1(a)(i)(A)(I)');
+      expect(
+        outline.reference([
+          {series: {number: 1, of: 1}, element: {number: 1, of: 1}},
+          {series: {number: 1, of: 1}, element: {number: 1, of: 1}},
+          {series: {number: 1, of: 1}, element: {number: 1, of: 1}},
+          {series: {number: 1, of: 1}, element: {number: 1, of: 1}},
+          {series: {number: 1, of: 1}, element: {number: 1, of: 1}}
+        ])
+      ).to.equal('Section 1(a)(i)(A)(I)');
     });
 
     it('repeats after the fifth level', function() {
-      expect(outline.reference([
-        {series: xofy(1, 1), element: xofy(1, 1)},
-        {series: xofy(1, 1), element: xofy(1, 1)},
-        {series: xofy(1, 1), element: xofy(1, 1)},
-        {series: xofy(1, 1), element: xofy(1, 1)},
-        {series: xofy(1, 1), element: xofy(1, 1)},
-        {series: xofy(1, 1), element: xofy(1, 1)}
-      ]))
-        .to.equal('Section 1(a)(i)(A)(I)(a)');
+      expect(
+        outline.reference([
+          {series: {number: 1, of: 1}, element: {number: 1, of: 1}},
+          {series: {number: 1, of: 1}, element: {number: 1, of: 1}},
+          {series: {number: 1, of: 1}, element: {number: 1, of: 1}},
+          {series: {number: 1, of: 1}, element: {number: 1, of: 1}},
+          {series: {number: 1, of: 1}, element: {number: 1, of: 1}},
+          {series: {number: 1, of: 1}, element: {number: 1, of: 1}}
+        ])
+      ).to.equal('Section 1(a)(i)(A)(I)(a)');
     });
   });
 });
